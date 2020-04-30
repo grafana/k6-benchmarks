5d77930def936b75f129d1df4b43daf1dadee7c2
Pawel Suwala <pawel.suwala@gmail.com>
2020-04-30 18:22:40 +0200

    made the RPS test more aggressive

--- scripts/RPS-optimized.es5.js
+++ scripts/RPS-optimized.es5.js
@@ -89,7 +89,7 @@
     };
     __webpack_exports__["default"] = function() {
         for (var i = 0; i < 20; i++) {
-            k6_http__WEBPACK_IMPORTED_MODULE_0___default.a.batch([ [ "GET", "http://pawel.staging.loadimpact.com/static/logo.svg?url=v1" ], [ "GET", "http://pawel.staging.loadimpact.com/static/logo.svg?url=v2" ], [ "GET", "http://pawel.staging.loadimpact.com/static/logo.svg?url=v3" ] ]);
+            k6_http__WEBPACK_IMPORTED_MODULE_0___default.a.batch([ [ "GET", "http://34.249.156.120/static/logo.svg?ip=1" ], [ "GET", "http://34.250.250.204/static/logo.svg?ip=2" ], [ "GET", "http://52.31.47.157/static/logo.svg?ip=3" ], [ "GET", "http://54.72.16.236/static/logo.svg?ip=4" ], [ "GET", "http://54.76.206.198/static/logo.svg?ip=5" ], [ "GET", "http://99.81.83.131/static/logo.svg?ip=6" ] ]);
         }
         Object(k6__WEBPACK_IMPORTED_MODULE_1__["sleep"])(.1);
     };
--- scripts/RPS-optimized.js
+++ scripts/RPS-optimized.js
@@ -17,13 +17,16 @@ export let options = {
 };
 
 export default function() {
-    // relentless hammering of these 3 URLs.
+    // relentless hammering of these 6 URLs.
     // 20 batches per iteration.
     for(let i=0; i<20; i++){
         http.batch([
-            ['GET', "http://pawel.staging.loadimpact.com/static/logo.svg?url=v1"],
-            ['GET', "http://pawel.staging.loadimpact.com/static/logo.svg?url=v2"],
-            ['GET', "http://pawel.staging.loadimpact.com/static/logo.svg?url=v3"],
+            ['GET', "http://34.249.156.120/static/logo.svg?ip=1"],
+            ['GET', "http://34.250.250.204/static/logo.svg?ip=2"],
+            ['GET', "http://52.31.47.157/static/logo.svg?ip=3"],
+            ['GET', "http://54.72.16.236/static/logo.svg?ip=4"],
+            ['GET', "http://54.76.206.198/static/logo.svg?ip=5"],
+            ['GET', "http://99.81.83.131/static/logo.svg?ip=6"],
         ]);
     }
   sleep(0.1); // 100ms sleep between iterations.
