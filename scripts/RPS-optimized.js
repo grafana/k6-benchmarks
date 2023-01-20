bf280628d5be3df8e9fbeca91b5ce4dcb23cb493
Ivan Mirić <ivan.miric@grafana.com>
2023-01-20 13:21:31 +0100

    Update host for RPS-optimized script

--- scripts/RPS-optimized.js
+++ scripts/RPS-optimized.js
@@ -21,12 +21,12 @@ export default function() {
     // 20 batches per iteration.
     for(let i=0; i<20; i++){
         http.batch([
-            ['GET', "http://34.249.156.120/static/logo.svg?ip=1"],
-            ['GET', "http://34.250.250.204/static/logo.svg?ip=2"],
-            ['GET', "http://52.31.47.157/static/logo.svg?ip=3"],
-            ['GET', "http://54.72.16.236/static/logo.svg?ip=4"],
-            ['GET', "http://54.76.206.198/static/logo.svg?ip=5"],
-            ['GET', "http://99.81.83.131/static/logo.svg?ip=6"],
+            ['GET', "https://pawel-test-nlb.staging.loadimpact.com/?req=1"],
+            ['GET', "https://pawel-test-nlb.staging.loadimpact.com/?req=2"],
+            ['GET', "https://pawel-test-nlb.staging.loadimpact.com/?req=3"],
+            ['GET', "https://pawel-test-nlb.staging.loadimpact.com/?req=4"],
+            ['GET', "https://pawel-test-nlb.staging.loadimpact.com/?req=5"],
+            ['GET', "https://pawel-test-nlb.staging.loadimpact.com/?req=6"],
         ]);
     }
   sleep(0.1); // 100ms sleep between iterations.
