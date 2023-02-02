5ec0af1d41b29e0b64903d5de2c9947d03b84f9c
Ivan Mirić <ivan.miric@grafana.com>
2023-02-02 16:05:15 +0100

    Use just http.get() for RPS-optimized test
    We only have a single IP serving the app, so with http.batch() we would
    quickly exhaust all ports to the same IP.

--- scripts/RPS-optimized.js
+++ scripts/RPS-optimized.js
@@ -4,7 +4,6 @@
 */
 
 import http from "k6/http";
-import { sleep } from "k6";
 
 export let options = {
   discardResponseBodies: true,
@@ -17,17 +16,5 @@ export let options = {
 };
 
 export default function() {
-  // relentless hammering of these 6 URLs.
-  // 20 batches per iteration.
-  for(let i=0; i<20; i++){
-    http.batch([
-      ['GET', "http://34.249.156.120/static/logo.svg?ip=1"],
-      ['GET', "http://34.250.250.204/static/logo.svg?ip=2"],
-      ['GET', "http://52.31.47.157/static/logo.svg?ip=3"],
-      ['GET', "http://54.72.16.236/static/logo.svg?ip=4"],
-      ['GET', "http://54.76.206.198/static/logo.svg?ip=5"],
-      ['GET', "http://99.81.83.131/static/logo.svg?ip=6"],
-    ]);
-  }
-  sleep(0.1); // 100ms sleep between iterations.
+  http.get('http://test.staging.k6.io/static/logo.svg');
 }
