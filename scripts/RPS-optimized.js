f69fd193e28ad81bd25bde2235b0a85f35eaddc4
Ivan Mirić <ivan.miric@grafana.com>
2023-02-02 15:42:12 +0100

    Update URLs in RPS-optimized script

--- scripts/RPS-optimized.js
+++ scripts/RPS-optimized.js
@@ -7,28 +7,27 @@ import http from "k6/http";
 import { sleep } from "k6";
 
 export let options = {
-	discardResponseBodies: true,
-    ext: {
-        loadimpact: {
-            name: `${__ENV.TEST_NAME}` || "RPS hammering test",
-            projectID: 3478725,
-        }
+  discardResponseBodies: true,
+  ext: {
+    loadimpact: {
+      name: `${__ENV.TEST_NAME}` || "RPS hammering test",
+      projectID: 3624575,
     }
+  }
 };
 
 export default function() {
-  http.get('https://pawel-test-nlb.staging.loadimpact.com/');
-    // relentless hammering of these 6 URLs.
-    // 20 batches per iteration.
-    // for(let i=0; i<20; i++){
-    //     http.batch([
-    //         ['GET', "https://pawel-test-nlb.staging.loadimpact.com/?req=1"],
-    //         ['GET', "https://pawel-test-nlb.staging.loadimpact.com/?req=2"],
-    //         ['GET', "https://pawel-test-nlb.staging.loadimpact.com/?req=3"],
-    //         ['GET', "https://pawel-test-nlb.staging.loadimpact.com/?req=4"],
-    //         ['GET', "https://pawel-test-nlb.staging.loadimpact.com/?req=5"],
-    //         ['GET', "https://pawel-test-nlb.staging.loadimpact.com/?req=6"],
-    //     ]);
-    // }
-  // sleep(0.1); // 100ms sleep between iterations.
+  // relentless hammering of these 6 URLs.
+  // 20 batches per iteration.
+  for(let i=0; i<20; i++){
+    http.batch([
+      ['GET', 'http://54.171.167.61:49203/static/logo.svg?ip=1'],
+      ['GET', 'http://34.244.55.142:49153/static/logo.svg?ip=2'],
+      ['GET', 'http://3.252.249.11:49203/static/logo.svg?ip=3'],
+      ['GET', 'http://54.220.37.194:49203/static/logo.svg?ip=4'],
+      ['GET', 'http://3.250.15.152:49153/static/logo.svg?ip=5'],
+      ['GET', 'http://3.250.35.227:49203/static/logo.svg?ip=6'],
+    ]);
+  }
+  sleep(0.1); // 100ms sleep between iterations.
 }
