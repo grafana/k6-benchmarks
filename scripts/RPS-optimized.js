dbcadc65331aac9da42adc20265cca95c04df169
Ivan Mirić <ivan.miric@grafana.com>
2023-02-02 16:05:15 +0100

    Use just http.get() for RPS-optimized test

--- scripts/RPS-optimized.js
+++ scripts/RPS-optimized.js
@@ -4,7 +4,6 @@
 */
 
 import http from "k6/http";
-import { sleep } from "k6";
 
 export let options = {
   discardResponseBodies: true,
@@ -17,17 +16,5 @@ export let options = {
 };
 
 export default function() {
-  // relentless hammering of these 6 URLs.
-  // 20 batches per iteration.
-  for(let i=0; i<20; i++){
-    http.batch([
-      ['GET', 'http://54.171.167.61:49203/static/logo.svg?ip=1'],
-      ['GET', 'http://34.244.55.142:49153/static/logo.svg?ip=2'],
-      ['GET', 'http://3.252.249.11:49203/static/logo.svg?ip=3'],
-      ['GET', 'http://54.220.37.194:49203/static/logo.svg?ip=4'],
-      ['GET', 'http://3.250.15.152:49153/static/logo.svg?ip=5'],
-      ['GET', 'http://3.250.35.227:49203/static/logo.svg?ip=6'],
-    ]);
-  }
-  sleep(0.1); // 100ms sleep between iterations.
+  http.get('http://test.staging.k6.io/static/logo.svg');
 }
