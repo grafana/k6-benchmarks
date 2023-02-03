b49b05dfa44f43ef1123ede0bfc76e54ca6f0b12
Ivan Mirić <ivan.miric@grafana.com>
2023-02-03 18:20:05 +0100

    Update file upload script

--- scripts/file-upload.js
+++ scripts/file-upload.js
@@ -1,21 +1,21 @@
-import http from "k6/http";
-import { sleep } from "k6";
+import http from 'k6/http';
+import { sleep } from 'k6';
 
 var data = {
-    file: http.file(open("./26MB.zip", "b"), "26MB.zip")
+    file: http.file(open('./26MB.zip', 'b'), '26MB.zip')
 };
 
 export let options = {
 	discardResponseBodies: true,
     ext: {
         loadimpact: {
-            name: `${__ENV.TEST_NAME}` || "Data transfer test",
-            projectID: 3478725,
+            name: `${__ENV.TEST_NAME}` || 'Data transfer test',
+            projectID: 3624575,
         }
     }
 };
 
 export default function() {
-  var res = http.post("https://pawel.staging.loadimpact.com/my_messages.php", data);
+  var res = http.post('http://test.staging.k6.io/my_messages.php', data);
   sleep(1);
 }
