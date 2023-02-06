632ca1dbed72bd7a5440f3f4112c538056ef548d
Ivan Mirić <ivan.miric@grafana.com>
2023-02-06 15:52:07 +0100

    Use 2 space indentation in JS scripts

--- scripts/super-quick.js
+++ scripts/super-quick.js
@@ -2,7 +2,7 @@ import http from "k6/http";
 import { sleep } from "k6";
 
 export let options = {
-	discardResponseBodies: true,
+  discardResponseBodies: true,
 };
 
 export default function() {
--- scripts/website.js
+++ scripts/website.js
@@ -9,17 +9,17 @@ let checkFailureRate = new Rate("check_failure_rate");
 let timeToFirstByte = new Trend("time_to_first_byte", true);
 
 export let options = {
-    thresholds: {
-        "http_req_duration": ["p(95)<500"],
-        "http_req_duration{staticAsset:yes}": ["p(95)<100"],
-        "check_failure_rate": ["rate<0.3"]
-    },
-    ext: {
-        loadimpact: {
-            name: `${__ENV.TEST_NAME}` || "AWS EC2 Hardware Testing",
-            projectID: 3478725,
-        }
+  thresholds: {
+    "http_req_duration": ["p(95)<500"],
+    "http_req_duration{staticAsset:yes}": ["p(95)<100"],
+    "check_failure_rate": ["rate<0.3"]
+  },
+  ext: {
+    loadimpact: {
+      name: `${__ENV.TEST_NAME}` || "AWS EC2 Hardware Testing",
+      projectID: 3478725,
     }
+  }
 };
 
 function randomIntBetween(min, max) { // min and max included
@@ -28,75 +28,75 @@ function randomIntBetween(min, max) { // min and max included
 
 let loginData = {
 
-    "users": [
-        { "username": "admin", "password": "123" },
-        { "username": "test", "password": "1234" },
-        { "username": "invaliduser", "password": "password"}
-    ]
+  "users": [
+    { "username": "admin", "password": "123" },
+    { "username": "test", "password": "1234" },
+    { "username": "invaliduser", "password": "password"}
+  ]
 };
 
 export default function() {
-    group("Front page", function() {
-        let res = http.get(http.url`http://test.k6.io/?ts=${Math.round(randomIntBetween(1,2000))}`, { tags: { name: "http://test.k6.io/?ts=*"}});
+  group("Front page", function() {
+    let res = http.get(http.url`http://test.k6.io/?ts=${Math.round(randomIntBetween(1,2000))}`, { tags: { name: "http://test.k6.io/?ts=*"}});
 
-        let checkRes = check(res, {
-            "Homepage body size is 11026 bytes": (r) => r.status === 200 && r.body.length === 11026,
-            "Homepage welcome header present": (r) => r.status === 200 && r.body.indexOf("Welcome to the k6.io demo site!") !== -1
-        });
-
-        checkFailureRate.add(!checkRes);
-
-        timeToFirstByte.add(res.timings.waiting, { ttfbURL: res.url });
-
-        group("Static assets", function() {
-            let res = http.batch([
-                ["GET", "http://test.k6.io/static/css/site.css", {}, { tags: { staticAsset: "yes" } }],
-                ["GET", "http://test.k6.io/static/js/prisms.js", {}, { tags: { staticAsset: "yes" } }]
-            ]);
-            checkRes = check(res[0], {
-                "Is stylesheet 4859 bytes?": (r) => r.status === 200 && r.body.length === 4859,
-            });
-
-            // Record check failures
-            checkFailureRate.add(!checkRes);
-
-            // Record time to first byte and tag it with the URL to be able to filter the results in Insights
-            timeToFirstByte.add(res[0].timings.waiting, { ttfbURL: res[0].url, staticAsset: "yes" });
-            timeToFirstByte.add(res[1].timings.waiting, { ttfbURL: res[1].url, staticAsset: "yes" });
-        });
-
+    let checkRes = check(res, {
+      "Homepage body size is 11026 bytes": (r) => r.status === 200 && r.body.length === 11026,
+      "Homepage welcome header present": (r) => r.status === 200 && r.body.indexOf("Welcome to the k6.io demo site!") !== -1
     });
 
-    sleep(5);
+    checkFailureRate.add(!checkRes);
 
-    group("Login", function() {
-        let res = http.get("http://test.k6.io/my_messages.php");
-        let checkRes = check(res, {
-            "Users should not be auth'd. Is unauthorized header present?": (r) => r.status === 200 && r.body.indexOf("Unauthorized") !== -1
-        });
+    timeToFirstByte.add(res.timings.waiting, { ttfbURL: res.url });
 
-        // Record check failures
-        checkFailureRate.add(!checkRes);
+    group("Static assets", function() {
+      let res = http.batch([
+        ["GET", "http://test.k6.io/static/css/site.css", {}, { tags: { staticAsset: "yes" } }],
+        ["GET", "http://test.k6.io/static/js/prisms.js", {}, { tags: { staticAsset: "yes" } }]
+      ]);
+      checkRes = check(res[0], {
+        "Is stylesheet 4859 bytes?": (r) => r.status === 200 && r.body.length === 4859,
+      });
 
-        let position = Math.floor(Math.random()*loginData.users.length);
-        let credentials = loginData.users[position];
+      // Record check failures
+      checkFailureRate.add(!checkRes);
 
-        res = http.post("http://test.k6.io/login.php", { login: credentials.username, password: credentials.password, redir: '1' });
-        checkRes = check(res, {
-            "is logged in welcome header present": (r) => r.status === 200 && r.body.indexOf("Welcome, admin!") !== -1
-        });
-
-        // Record successful logins
-        if (checkRes) {
-            successfulLogins.add(1);
-        }
-
-        // Record check failures
-        checkFailureRate.add(!checkRes, { page: "login" });
-
-        // Record time to first byte and tag it with the URL to be able to filter the results in Insights
-        timeToFirstByte.add(res.timings.waiting, { ttfbURL: res.url });
+      // Record time to first byte and tag it with the URL to be able to filter the results in Insights
+      timeToFirstByte.add(res[0].timings.waiting, { ttfbURL: res[0].url, staticAsset: "yes" });
+      timeToFirstByte.add(res[1].timings.waiting, { ttfbURL: res[1].url, staticAsset: "yes" });
     });
 
-    sleep(5);
+  });
+
+  sleep(5);
+
+  group("Login", function() {
+    let res = http.get("http://test.k6.io/my_messages.php");
+    let checkRes = check(res, {
+      "Users should not be auth'd. Is unauthorized header present?": (r) => r.status === 200 && r.body.indexOf("Unauthorized") !== -1
+    });
+
+    // Record check failures
+    checkFailureRate.add(!checkRes);
+
+    let position = Math.floor(Math.random()*loginData.users.length);
+    let credentials = loginData.users[position];
+
+    res = http.post("http://test.k6.io/login.php", { login: credentials.username, password: credentials.password, redir: '1' });
+    checkRes = check(res, {
+      "is logged in welcome header present": (r) => r.status === 200 && r.body.indexOf("Welcome, admin!") !== -1
+    });
+
+    // Record successful logins
+    if (checkRes) {
+      successfulLogins.add(1);
+    }
+
+    // Record check failures
+    checkFailureRate.add(!checkRes, { page: "login" });
+
+    // Record time to first byte and tag it with the URL to be able to filter the results in Insights
+    timeToFirstByte.add(res.timings.waiting, { ttfbURL: res.url });
+  });
+
+  sleep(5);
 }
